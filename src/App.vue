3aa7953b69d53a8c9da52952c0906d4f4b39f876
minterger <minterger@gmail.com>
2022-01-14 16:47:51 -0300

    add states global and import new components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
+import { provide, ref } from 'vue';
 import UploadComponent from './components/UploadComponent.vue';
+import LoadUploading from './components/LoadUploading.vue';
+import InfoUpload from './components/InfoUpload.vue';
+
+const isUploadActive = ref(false);
+const isUploadFinished = ref(false);
+
+const dataImg = ref({});
+
+provide('isUploadActive', isUploadActive);
+provide('isUploadFinished', isUploadFinished);
+provide('dataImg', dataImg);
 </script>
 
 <template>
   <main class="min-h-screen bg-slate-50 flex justify-center items-center	px-2">
-    <upload-component />
+    <upload-component v-if="!isUploadActive && !isUploadFinished" />
+    <load-uploading v-if="isUploadActive"/>
+    <info-upload v-if="isUploadFinished"/>
   </main>
 </template>
