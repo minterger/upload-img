bde3d21d89603a7c42f6dca5479a737537a5dcaa
minterger <minterger@gmail.com>
2022-01-14 16:47:15 -0300

    Create LoadUploading.vue

---
+++ src/components/LoadUploading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="max-w-sm w-full p-5 rounded-lg bg-white shadow-lg md:p-8"
+  >
+  <h1 class="mt-3 mb-3 text-xl">Uploading...</h1>
+  <div class="rounded-lg bg-slate-50 w-full h-2 overflow-hidden">
+    <div class="w-20 h-full rounded-lg bg-blue-600 animation-load"></div>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.animation-load {
+  animation: loading 2.5s linear infinite;
+}
+
+@keyframes loading {
+  0% {
+    transform: translateX(-5rem);
+  }
+  100% {
+    transform: translateX(500%);
+  }
+}
+</style>
