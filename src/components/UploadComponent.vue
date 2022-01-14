fc0a5d2fb1356bdfd0af3463267973f038b635de
minterger <minterger@gmail.com>
2022-01-14 16:48:22 -0300

    add states globals, fix bugs and finish component

--- src/components/UploadComponent.vue
+++ src/components/UploadComponent.vue
@@ -1,6 +1,11 @@
 <script setup>
+import axios from 'axios';
 import { ref } from "@vue/reactivity";
-import { onMounted } from "@vue/runtime-core";
+import { inject } from "@vue/runtime-core";
+
+const isUploadActive = inject('isUploadActive');
+const isUploadFinished = inject('isUploadFinished');
+const dataImg = inject('dataImg');
 
 const dropzoneIsActive = ref(false);
 const file = ref(null);
@@ -9,28 +14,50 @@ const toggleDrag = () => {
   if (!file.value) dropzoneIsActive.value = !dropzoneIsActive.value;
 };
 
-const uploadImg = (e) => {
-  file.value = e.value.files[0];
-  console.log(file.value);
+const uploadButtonImg = (e) => {
+  file.value = e.target.files[0];
+  uploadImg();
 };
 
 const dropFile = (e) => {
   file.value = e.dataTransfer.files[0];
-  console.log(file.value);
+  uploadImg();
 };
+
+const uploadImg = () => {
+  if (file.value.type.match("image.*")) {
+    const formData = new FormData();
+    formData.append("image", file.value);
+    isUploadActive.value = true;
+    axios.post("https://api.imgbb.com/1/upload?key=45f666aa12a2edd235f9e6bf8378d22d", formData)
+      .then((res) => {
+        dataImg.value = res.data;
+        isUploadActive.value = false;
+        isUploadFinished.value = true;
+      })
+      .catch((err) => {
+        console.log(err);
+        isUploadActive.value = false;
+      });
+  } else {
+    alert("File is not an image");
+    file.value = null;
+    dropzoneIsActive.value = false;
+  }
+}
 </script>
 
 <template>
   <div
     class="max-w-sm w-full p-5 rounded-lg bg-white shadow-lg flex items-center flex-col md:p-8"
   >
-    <h1 class="mt-3 mb-3 text-2xl">Upload your image</h1>
+    <h1 class="mt-3 mb-3 text-xl">Upload your image</h1>
     <span class="text-xs mt-2 mb-2 text-slate-600"
       >File should be Jpeg, Png,...</span
     >
     <div
-      @dragenter.prevent="toggleDrag"
-      @dragleave.prevent="toggleDrag"
+      @dragenter.prevent="toggleDrag()"
+      @dragleave.prevent="toggleDrag()"
       @dragover.prevent=""
       @drop.prevent="dropFile"
       class="border border-blue-500 border-dashed rounded-lg w-full h-52 flex items-center flex-col mt-4 bg-slate-50 transition-all duration-200"
@@ -57,7 +84,7 @@ const dropFile = (e) => {
         type="file"
         class="hidden"
         id="file"
-        @change="uploadImg"
+        @change.prevent="uploadButtonImg"
       />
     </label>
   </div>
