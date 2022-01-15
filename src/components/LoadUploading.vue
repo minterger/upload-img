79363fd5da59bc98f5146420ed2bec61bd87fb20
minterger <minterger@gmail.com>
2022-01-14 22:00:17 -0300

    fix uploading bar

--- src/components/LoadUploading.vue
+++ src/components/LoadUploading.vue
@@ -2,9 +2,9 @@
   <div
     class="max-w-sm w-full p-5 rounded-xl bg-white shadow-lg md:p-8"
   >
-  <h1 class="mt-3 mb-3 text-xl">Uploading...</h1>
+  <h1 class="mb-3 text-xl">Uploading...</h1>
   <div class="rounded-lg bg-slate-50 w-full h-2 overflow-hidden">
-    <div class="w-20 h-full rounded-lg bg-blue-600 animation-load"></div>
+    <div class="w-1/3 h-full rounded-lg bg-blue-600 animation-load"></div>
   </div>
   </div>
 </template>
@@ -16,10 +16,10 @@
 
 @keyframes loading {
   0% {
-    transform: translateX(-5rem);
+    transform: translateX(-100%);
   }
   100% {
-    transform: translateX(500%);
+    transform: translateX(300%);
   }
 }
 </style>
